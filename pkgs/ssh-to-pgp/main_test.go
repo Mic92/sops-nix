0d885b439f9bd178b5932f19b815075c0b898380
Andreas Fuchs <asf@boinkor.net>
2020-07-22 09:11:42 -0400

    Create a temporary GNUPGHOME dir
    This should prevent the paths from getting unwieldy, we'll see.

--- pkgs/ssh-to-pgp/main_test.go
+++ pkgs/ssh-to-pgp/main_test.go
@@ -27,6 +27,10 @@ func TestCli(t *testing.T) {
 	ok(t, err)
 	defer os.RemoveAll(tempdir)
 
+	gpgHome := path.Join(tempdir, "gpg-home")
+	gpgEnv := append(os.Environ(), fmt.Sprintf("GNUPGHOME=%s", gpgHome))
+	ok(t, os.Mkdir(gpgHome, os.FileMode(0700)))
+
 	out := path.Join(tempdir, "out")
 	privKey := path.Join(assets, "id_rsa")
 	cmds := [][]string{
@@ -41,6 +45,7 @@ func TestCli(t *testing.T) {
 		cmd := exec.Command("gpg", "--with-fingerprint", "--show-key", out)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
+		cmd.Env = gpgEnv
 		ok(t, cmd.Run())
 	}
 }
