664c4d7101eff5f25ca386c400f167313cc304ba
Jörg Thalheim <joerg@thalheim.io>
2022-05-25 08:40:52 +0200

    also build ci for 22.05

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   description = "Integrates sops into nixos";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.nixpkgs-21_11.url = "github:NixOS/nixpkgs/release-21.11";
+  inputs.nixpkgs-22_05.url = "github:NixOS/nixpkgs/release-22.05";
   nixConfig.extra-substituters = ["https://cache.garnix.io"];
   nixConfig.extra-trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
   outputs = {
     self,
     nixpkgs,
-    nixpkgs-21_11
+    nixpkgs-21_11,
+    nixpkgs-22_05
   }: let
     systems = [
       "x86_64-linux"
@@ -19,7 +21,9 @@
       "armv7l-linux"
     ];
     forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    suffix-21_11 = attrs: nixpkgs.lib.mapAttrs' (name: value: nixpkgs.lib.nameValuePair (name + "-21_11") value) attrs;
+    suffix-version = version: attrs: nixpkgs.lib.mapAttrs' (name: value: nixpkgs.lib.nameValuePair (name + version) value) attrs;
+    suffix-22_05 = suffix-version "-22_05";
+    suffix-21_11 = suffix-version "-21_11";
   in {
     overlay = final: prev: let
       localPkgs = import ./default.nix {pkgs = final;};
@@ -40,8 +44,16 @@
         packages-21_11 = import ./default.nix {
           pkgs = import nixpkgs-21_11 {inherit system;};
         };
+        packages-22_05 = import ./default.nix {
+          pkgs = import nixpkgs-22_05 {inherit system;};
+        };
         tests-21_11 = packages-21_11.sops-install-secrets.tests;
-      in tests // (suffix-21_11 tests-21_11) // (suffix-21_11 packages-21_11));
+        tests-22_05 = packages-22_05.sops-install-secrets.tests;
+      in tests //
+         (suffix-21_11 tests-21_11) //
+         (suffix-21_11 packages-21_11) //
+         (suffix-22_05 tests-22_05) //
+         (suffix-22_05 packages-22_05));
 
     defaultPackage = forAllSystems (system: self.packages.${system}.sops-init-gpg-key);
     devShell = forAllSystems (
