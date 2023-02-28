8da6068e916e46ab6bcbd0fcfdffea2d352454f1
Jörg Thalheim <joerg@thalheim.io>
2023-02-28 09:29:06 +0100

    bump go version

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }: let
-  vendorSha256 = "sha256-6WyyR1UvsBZDIgtZ7H9wsXe6M5MY2A/MR9M6c/31v/w=";
+  vendorSha256 = "sha256-3rU6jyy/fSjj2jjK2nb1kcI1xnxUp4pzrakdY1lvRz4=";
 
   buildGoModule = if pkgs.lib.versionOlder pkgs.go.version "1.18" then pkgs.buildGo118Module else pkgs.buildGoModule;
   sops-install-secrets = pkgs.callPackage ./pkgs/sops-install-secrets {
