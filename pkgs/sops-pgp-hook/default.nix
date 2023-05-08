b84b3177a1031f70e5d5c20aa8c748dade477385
Jörg Thalheim <joerg@thalheim.io>
2023-05-08 06:48:56 +0200

    fix makeSetupHook's also for older nixos release

--- pkgs/sops-pgp-hook/default.nix
+++ pkgs/sops-pgp-hook/default.nix
@@ -1,9 +1,13 @@
-{ stdenv, makeSetupHook, gnupg, sops, nix }:
+{ makeSetupHook, gnupg, sops, lib }:
 
+let
+  # FIXME: drop after 23.05
+  propagatedBuildInputs = if (lib.versionOlder (lib.versions.majorMinor lib.version) "23.05") then "deps" else "propagatedBuildInputs";
+in
 (makeSetupHook {
   name = "sops-pgp-hook";
   substitutions = {
     gpg = "${gnupg}/bin/gpg";
   };
-  propagatedBuildInputs = [ sops gnupg ];
+  ${propagatedBuildInputs} = [ sops gnupg ];
 } ./sops-pgp-hook.bash)
