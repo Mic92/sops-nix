9190dee4086c41debe0e71e055f4115eb3bca2dd
Jörg Thalheim <joerg@thalheim.io>
2024-11-17 18:42:20 +0100

    sops-pgp-hook: set parallel and helper

--- pkgs/sops-pgp-hook/hook_test.go
+++ pkgs/sops-pgp-hook/hook_test.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/exec"
 	"path"
-	"path/filepath"
 	"runtime"
 	"strings"
 	"testing"
@@ -14,14 +13,17 @@ import (
 
 // ok fails the test if an err is not nil.
 func ok(tb testing.TB, err error) {
+	tb.Helper()
+
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		fmt.Printf("\033[31munexpected error: %s\033[39m\n\n", err.Error())
 		tb.FailNow()
 	}
 }
 
 func TestShellHook(t *testing.T) {
+	t.Parallel()
+
 	assets := os.Getenv("TEST_ASSETS")
 	if assets == "" {
 		_, filename, _, _ := runtime.Caller(0)
