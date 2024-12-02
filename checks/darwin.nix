fefa19b6a3cbac872859606b2c8846781d782d77
Jörg Thalheim <joerg@thalheim.io>
2024-12-02 09:22:56 +0100

    {darwin,home-manager}: add example template

--- checks/darwin.nix
+++ checks/darwin.nix
@@ -1,10 +1,16 @@
-
+{
+  config,
+  ...
+}:
 {
   imports = [
     ../modules/nix-darwin/default.nix
   ];
   documentation.enable = false;
   sops.secrets.test_key = { };
+  sops.templates."template.toml".content = ''
+    password = "${config.sops.placeholder.test_key}";
+  '';
   sops.defaultSopsFile = ../pkgs/sops-install-secrets/test-assets/secrets.yaml;
   sops.age.generateKey = true;
   system.stateVersion = 5;
