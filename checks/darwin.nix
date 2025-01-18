787afce414bcce803b605c510b60bf43c11f4b55
David Kowis <david@kow.is>
2025-01-17 23:23:32 -0600

    add uid and gid to templates

--- checks/darwin.nix
+++ checks/darwin.nix
@@ -8,9 +8,23 @@
   ];
   documentation.enable = false;
   sops.secrets.test_key = { };
-  sops.templates."template.toml".content = ''
-    password = "${config.sops.placeholder.test_key}";
-  '';
+  sops.templates."template.toml" = {
+    content = ''
+      password = "${config.sops.placeholder.test_key}";
+    '';
+  };
+  sops.templates."template-with-uid.toml" = {
+    content = ''
+      password = "${config.sops.placeholder.test_key}";
+    '';
+    uid = 1000;
+  };
+  sops.templates."template-with-gid.toml" = {
+    content = ''
+      password = "${config.sops.placeholder.test_key}";
+    '';
+    gid = 1000;
+  };
   sops.defaultSopsFile = ../pkgs/sops-install-secrets/test-assets/secrets.yaml;
   sops.age.generateKey = true;
   system.stateVersion = 5;
