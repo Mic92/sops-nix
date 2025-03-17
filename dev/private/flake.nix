9520344f1f788c49591df03de2e25fe48364e0bd
Jörg Thalheim <joerg@thalheim.io>
2025-03-17 11:00:34 +0100

    use nixpkgs-unstable for home-manager/darwin-nix/treefmt-nix

--- dev/private/flake.nix
+++ dev/private/flake.nix
@@ -1,15 +1,18 @@
 {
   description = "private inputs";
-  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+  inputs.nixpkgs-stable.url = "git+https://github.com/NixOS/nixpkgs?shallow=1&ref=nixos-24.11";
+
+  inputs.nixpkgs-unstable.url = "git+https://github.com/NixOS/nixpkgs?shallow=1&ref=nixpkgs-unstable";
 
   inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
-  inputs.treefmt-nix.inputs.nixpkgs.follows = "nixpkgs-stable";
+  inputs.treefmt-nix.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
   inputs.nix-darwin.url = "github:LnL7/nix-darwin";
-  inputs.nix-darwin.inputs.nixpkgs.follows = "nixpkgs-stable";
+  inputs.nix-darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
   inputs.home-manager.url = "github:nix-community/home-manager";
-  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs-stable";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
   outputs = _: { };
 }
